24cb76c63a8f0b29f7775b5bf86d2851b2b2b2c5
Minji Son <midday2612@gmail.com>
2025-04-01 20:14:57 +0900

    fix:config server 인식 문제 해결

--- src/main/java/com/amcamp/RecommendationApplication.java
+++ src/main/java/com/amcamp/RecommendationApplication.java
@@ -2,12 +2,15 @@ package com.amcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
-public class SyncfitApplication {
+public class RecommendationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SyncfitApplication.class, args);
+        SpringApplication.run(RecommendationApplication.class, args);
     }
 
 }
